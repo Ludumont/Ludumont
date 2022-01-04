0e6b49c2a48cb62d338a6d6ceb4b8193eb3efad2
Lucas Dumont <dumontlucas@hotmail.com>
2022-01-04 22:07:36 +0100

    Petit travail maison J2

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldumont <ldumont@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/04 14:36:58 by ludumont          #+#    #+#             */
-/*   Updated: 2022/01/04 21:48:09 by ldumont          ###   ########.fr       */
+/*   Created: 2022/01/04 21:43:24 by ldumont           #+#    #+#             */
+/*   Updated: 2022/01/04 21:46:43 by ldumont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
+void    ft_bzero(void *s, size_t n)
 {
-	const unsigned char *s1p;
-	const unsigned char *s2p;
-
-	s1p = (const unsigned char *)s1;
-	s2p = (const unsigned char *)s2;
-	while (n-- > 0)
-	{
-		if(*s1p != *s2p)
-			return (*s1p - *s2p);
-		s1p++;
-		s2p++;
-	}
-	return (0);
+    ft_memset(s, 0, n);
 }
--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -3,16 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ludumont <ludumont@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ludumont <ludumont@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/04 14:46:31 by ludumont          #+#    #+#             */
-/*   Updated: 2022/01/04 15:17:05 by ludumont         ###   ########.fr       */
+/*   Created: 2022/01/04 22:04:20 by ludumont          #+#    #+#             */
+/*   Updated: 2022/01/04 22:04:44 by ludumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "libft.h"
 
-char *ft_strnstr(const char	*s1, const char *s2, size_t len)
+char *ft_strnstr(const char	*str, const char *to_find, size_t len)
 {
+    unsigned int n;
+    unsigned int i;
 
+    if(!*to_find)
+        return ((char*)str);
+    n = 0;
+	while (str[n] != '\0' && (size_t)n < len)
+	{
+		if (str[n] == to_find[0])
+		{
+			i = 1;
+			while (to_find[i] != '\0' && str[n + i] == to_find[i] &&
+					(size_t)(n + i) < len)
+				++i;
+			if (to_find[i] == '\0')
+				return ((char*)&str[n]);
+		}
+		n++;
+	}
+	return (0);
 }
